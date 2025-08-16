3461494cee701fef294fb74d33334812cc8e0bd5
margarita ramos <ramosmargarita2m@gmail.com>
2025-08-16 16:08:38 -0600

    Agregué las ventanas de la UI

---
+++ InterfazDeUsuarioUI/VentanaMascota.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InterfazDeUsuarioUI
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaMascota.xaml
+    /// </summary>
+    public partial class VentanaMascota : Window
+    {
+        public VentanaMascota()
+        {
+            InitializeComponent();
+        }
+    }
+}
